bd8a3ec4c3c4781f65f2fca3de126031f8246bef
Gary White <gary@iMac-2017.local>
2023-01-26 12:36:07 +0100

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function datingRange(age){
+    //return min-max
+}
+
+console.log(datingRange(17)); // "15-20"
+console.log(datingRange(40)); // "27-66"
+console.log(datingRange(15)); // "14-16"
+console.log(datingRange(35)); // "24-56"
+console.log(datingRange(10)); // "9-11"
